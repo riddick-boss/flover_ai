069fb52e6086e3e6d65114f6fc1aec8c7dd6bd9c
Pawel Kremienowski <Kremienowski33@gmail.com>
2024-02-19 14:29:28 +0100

    feat: image stream processing

--- lib/feature/recognizer/cubit/recognizer_cubit.dart
+++ lib/feature/recognizer/cubit/recognizer_cubit.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:camera/camera.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -8,15 +11,29 @@ class RecognizerCubit extends Cubit<RecognizerState> {
   RecognizerCubit()
       : super(const RecognizerState(
           recognizedFlowerName: null,
-        )) {
-    Future.delayed(const Duration(seconds: 10), () {
-      emit(state.copyWith(recognizedFlowerName: 'Tulip'));
-    });
-    Future.delayed(const Duration(seconds: 20), () {
-      emit(state.copyWith(recognizedFlowerName: null));
-    });
-    Future.delayed(const Duration(seconds: 30), () {
-      emit(state.copyWith(recognizedFlowerName: 'Rose'));
+        ));
+
+  bool _isDetecting = false;
+
+  void onNextImage(CameraImage image) {
+    if (!_isDetecting) {
+      _isDetecting = true;
+      log('detecting...');
+      final startTime = DateTime.now().millisecondsSinceEpoch;
+
+      _performDetection(image).then((value) {
+        final endTime = DateTime.now().millisecondsSinceEpoch;
+        final detectionTime = endTime - startTime;
+        log('detectionTime is: $detectionTime');
+        _isDetecting = false;
+      });
+    }
+  }
+
+  Future<void> _performDetection(CameraImage image) async {
+    await Future.delayed(const Duration(seconds: 5), () {
+      emit(state.copyWith(
+          recognizedFlowerName: '${DateTime.now().millisecondsSinceEpoch}'));
     });
   }
 }
