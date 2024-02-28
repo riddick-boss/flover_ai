02501993ea8843c6ce496985e27a7ce6aba4855a
Pawel Kremienowski <Kremienowski33@gmail.com>
2024-02-28 11:17:38 +0100

    feat: recognize with google mlkit

--- lib/feature/recognizer/cubit/recognizer_cubit.dart
+++ lib/feature/recognizer/cubit/recognizer_cubit.dart
@@ -1,8 +1,10 @@
 import 'dart:developer';
 
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
 
 part 'recognizer_state.dart';
 part 'recognizer_cubit.freezed.dart';
@@ -31,9 +33,30 @@ class RecognizerCubit extends Cubit<RecognizerState> {
   }
 
   Future<void> _performDetection(CameraImage image) async {
-    await Future.delayed(const Duration(seconds: 5), () {
-      emit(state.copyWith(
-          recognizedFlowerName: '${DateTime.now().millisecondsSinceEpoch}'));
-    });
+    final inputImage = InputImage.fromBytes(
+        bytes: image.planes.first.bytes,
+        metadata: InputImageMetadata(
+          size: Size(image.width.toDouble(), image.height.toDouble()),
+          rotation:
+              InputImageRotationValue.fromRawValue(0)!, // used only in Android
+          format: InputImageFormatValue.fromRawValue(
+              image.format.raw)!, // used only in iOS
+          bytesPerRow: image.planes.first.bytesPerRow,
+        )); // used only in iOS
+
+    final options = ObjectDetectorOptions(
+      classifyObjects: true,
+      mode: DetectionMode.stream,
+      multipleObjects: true,
+    );
+    final objectDetector = ObjectDetector(options: options);
+    final detectedObjects = await objectDetector.processImage(inputImage);
+    final label = detectedObjects.firstOrNull?.labels.firstOrNull;
+    if (label == null) {
+      emit(state.copyWith(recognizedFlowerName: null));
+      return;
+    }
+    final recogniezd = '${label.text} ${label.confidence}';
+    emit(state.copyWith(recognizedFlowerName: recogniezd));
   }
 }
